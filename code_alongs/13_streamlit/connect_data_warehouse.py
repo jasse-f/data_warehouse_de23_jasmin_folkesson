5f3cbf5c854c1638a76139d8d540a20ffb244a1d
Jasmin <SwedishSolidSnake@gmail.com>
2024-09-13 16:59:02 +0200

    fixed for streamlit cloud

--- code_alongs/13_streamlit/connect_data_warehouse.py
+++ code_alongs/13_streamlit/connect_data_warehouse.py
@@ -2,20 +2,41 @@ import os
 from dotenv import load_dotenv
 from snowflake.connector import connect
 import pandas as pd
+import streamlit as st
+
+
 
 def query_job_listings(query = 'SELECT * FROM mart_job_listings'):
-    load_dotenv()
+    if "SNOWFLAKE_USER" in st.secrets:
+        snowflake_user = st.secrets["SNOWFLAKE_USER"]
+        snowflake_password = st.secrets["SNOWFLAKE_PASSWORD"]
+        snowflake_account = st.secrets["SNOWFLAKE_ACCOUNT"]
+        snowflake_warehouse = st.secrets["SNOWFLAKE_WAREHOUSE"]
+        snowflake_database = st.secrets["SNOWFLAKE_DATABASE"]
+        snowflake_schema = st.secrets["SNOWFLAKE_SCHEMA"]
+        snowflake_role= st.secrets["SNOWFLAKE_ROLE"]
+
+    else:
+        load_dotenv()
+        snowflake_user = os.getenv("SNOWFLAKE_USER"),
+        snowflake_password = os.getenv("SNOWFLAKE_PASSWORD")
+        snowflake_account = os.getenv("SNOWFLAKE_ACCOUNT")
+        snowflake_warehouse = os.getenv("SNOWFLAKE_WAREHOUSE")
+        snowflake_database = os.getenv("SNOWFLAKE_DATABASE")
+        snowflake_schema = os.getenv("SNOWFLAKE_SCHEMA")
+        snowflake_role = os.getenv("SNOWFLAKE_ROLE")
 
     with connect(
-        user = os.getenv("SNOWFLAKE_USER"), 
-        password = os.getenv("SNOWFLAKE_PASSWORD"),
-        account = os.getenv("SNOWFLAKE_ACCOUNT"), 
-        warehouse = os.getenv("SNOWFLAKE_WAREHOUSE"),
-        database = os.getenv("SNOWFLAKE_DATABASE"),
-        schema = os.getenv("SNOWFLAKE_SCHEMA"),
-        role = os.getenv("SNOWFLAKE_ROLE")
+        user=snowflake_user,
+        password=snowflake_password,
+        account=snowflake_account,
+        warehouse=snowflake_warehouse,
+        database=snowflake_database,
+        schema=snowflake_schema,
+        role=snowflake_role
     ) as conn:
         
         df = pd.read_sql(query, conn)
         return df
+    
     
